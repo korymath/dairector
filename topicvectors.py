41bb6470a2bcc5e7820988f1b0882456313d821c
Kory Mathewson <korymath@google.com>
2018-11-11 16:36:46 -0700

    updating the vector training code

--- topicvectors.py
+++ topicvectors.py
@@ -1,26 +1,31 @@
 import sys
-import random
-import pickle
 import json
 import numpy
+import random
+import pickle
 import gensim.utils
+from tqdm import tqdm
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info('Building topic model with gensim...')
+random.seed(1017)
 
 
 def cos_dist(v1,v2):
     return 1 - numpy.dot(gensim.matutils.unitvec(v1), gensim.matutils.unitvec(v2))
-    
+
 def preprocess(m, texts):
     (model,trainset) = m
     result = []
     for t in texts:
         result.append(model.infer_vector(gensim.utils.simple_preprocess(t), steps=1000))
     return result
-    
+
 def closestn_v(m, t, options, n=5):
     (model,trainset) = m
     vec = model.infer_vector(gensim.utils.simple_preprocess(t), steps=1000)
-    #print(t, vec)
     result = None
     mindist = -1
     minat = None
@@ -30,11 +35,10 @@ def closestn_v(m, t, options, n=5):
         return cos_dist(vec, v)
     loptions.sort(key=vdist)
     return loptions[:n]
-    
+
 def closest_v(m, t, options):
     (model,trainset) = m
     vec = model.infer_vector(gensim.utils.simple_preprocess(t), steps=1000)
-    #print(t, vec)
     result = None
     mindist = -1
     minat = None
@@ -60,46 +64,18 @@ def closest(m, t, options):
             mindist = d
             minat = i
     return result,minat
-        
 
 def recall(m, t):
     (model,trainset) = m
     vec = model.infer_vector(gensim.utils.simple_preprocess(t))
     (sims,w) = model.docvecs.most_similar([vec], topn=len(model.docvecs))[0]
-    return trainset[sims],w 
-    
+    return trainset[sims], w
+
 def get_model(fname):
     f = open(fname, "rb")
     m = pickle.load(f)
     f.close()
     return m
-   
-
-def main(outf, docfs):
-    docs = set()
-    random.seed(4)
-    
-    for d in docfs:
-        f = open(d, "r")
-        stats = json.load(f)
-        f.close()
-        for s in stats["states"]:
-            if stats["states"][s]:
-                docs.add(stats["states"][s][0]["text"])
-    print("have", len(docs))
-    (model,trainset) = train(docs, outf)
-    for d in docfs:
-        f = open(d, "r")
-        stats = json.load(f)
-        f.close()
-        for s in stats["states"]:
-            if stats["states"][s]:
-                vec = list(map(float, list(model.infer_vector(gensim.utils.simple_preprocess(stats["states"][s][0]["text"]), steps=1000))))
-                stats["states"][s][0]["vec"] = vec
-        f = open(d.replace(".json", "_v.json"), "w")
-        json.dump(stats, f, indent=4)
-        f.close()
-    
 
 def train(docs, outf="model.bin"):
     docs = list(docs)
@@ -115,30 +91,91 @@ def train(docs, outf="model.bin"):
     documents = []
     for i,d in enumerate(trainset):
         documents.append(TaggedDocument(gensim.utils.simple_preprocess(d), [i]))
-    print(documents[:2])
-    (size, min_count, negative, iter, alpha, min_alpha, window) = (410, 2, 4, 900, 0.03, 0.01, 4) #(500, 2, 5, 150, 0.08, 0.02, 2)
-    model = Doc2Vec(size=size, dm=0, window=window, min_count=min_count, negative=negative, epochs=iter, alpha=alpha, min_alpha=min_alpha, workers=3)
+
+    # Topic model variables
+    size = 256
+    min_count = 2
+    negative = 4
+    iterations = 10
+    alpha = 0.03
+    min_alpha = 0.01
+    window = 4
+
+    # Train the model with DOC2VEC
+    model = Doc2Vec(
+        vector_size=size,
+        dm=0,
+        window=window,
+        min_count=min_count,
+        negative=negative,
+        epochs=iterations,
+        alpha=alpha,
+        min_alpha=min_alpha,
+        workers=3)
+
     model.build_vocab(documents)
-    model.train(documents, total_examples=model.corpus_count, epochs=model.iter)
+    model.train(documents,
+        total_examples=model.corpus_count,
+        epochs=model.iter)
+
+    logging.info('Saving model.')
     f = open(outf, "wb")
     pickle.dump((model,trainset), f)
     f.close()
+    logging.info('Model saved.')
+
+    # Track the performance on the training set
     correct = 0
     incorrect = 0
-    for i,t in enumerate(trainset):
-        (sims,w) = recall((model,trainset), t)
+    for i,t in enumerate(tqdm(trainset)):
+        (sims, w) = recall((model, trainset), t)
         if sims == t:
             correct += 1
         else:
             incorrect += 1
-        #print(sims, t)
-    print("performance:", correct*1.0/(correct + incorrect))
-    ranks = []
-    for t in testset:
-        (sims,w) = recall((model,trainset), t)
-        
-        print(t, "\n   closest is:\n", sims, "\n  dist:", w, "\n\n")
-    return (model,trainset)  
-        
+    logging.info("performance: {}".format(correct*1.0/(correct + incorrect + 0.00001)))
+
+    # Track the performance on the test set
+    test_correct = 0
+    test_incorrect = 0
+    for i,t in enumerate(tqdm(testset)):
+        (sims, w) = recall((model, trainset), t)
+        if sims == t:
+            correct += 1
+        else:
+            incorrect += 1
+    logging.info("test_performance: {}".format(test_correct*1.0/(test_correct + test_incorrect + 0.00001)))
+    # logging.info(t, "\n   closest is:\n", sims, "\n  dist:", w, "\n\n")
+    return (model, trainset)
+
+
+def main(outf, docfs):
+    """ Main run code for training a topic model."""
+    docs = set()
+    for d in docfs:
+        f = open(d, "r")
+        stats = json.load(f)
+        f.close()
+        for s in stats["states"]:
+            if stats["states"][s]:
+                docs.add(stats["states"][s][0]["text"])
+    # Train the model.
+    (model, _) = train(docs, outf)
+
+    # Use the newly trained model to make vectors of the docs
+    # Save the vectors to a file
+    for d in tqdm(docfs):
+        f = open(d, "r")
+        stats = json.load(f)
+        f.close()
+        for s in stats["states"]:
+            if stats["states"][s]:
+                vec = list(map(float, list(model.infer_vector(gensim.utils.simple_preprocess(stats["states"][s][0]["text"]), epochs=10))))
+                stats["states"][s][0]["vec"] = vec
+        f = open(d.replace(".json", "_v.json"), "w")
+        json.dump(stats, f, indent=4)
+        f.close()
+
+
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2:])
